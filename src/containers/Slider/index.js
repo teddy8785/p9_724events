387896373f5187a9c6c94886b0897da06671600b
teddy8587 <teddy.barieraud@gmail.com>
2024-10-16 20:24:36 +0200

    refactor modif src images slider pour deploiement

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -31,7 +31,7 @@ const Slider = () => {
           key={`${event.title}-${event.date}`}
           className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
         >
-          <img src={event.cover} alt={event.title} />
+          <img src={`${process.env.PUBLIC_URL}${event.cover}`} alt={event.title} />
           <div className="SlideCard__descriptionContainer">
             <div className="SlideCard__description">
               <h3>{event.title}</h3>
